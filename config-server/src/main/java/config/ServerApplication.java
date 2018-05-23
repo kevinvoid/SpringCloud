bc981bf199fae2901d190597b743f653010a82af
kevin <yangpingan888@163.com>
2018-05-23 16:40:02 +0800

    update

--- config-server/src/main/java/config/ServerApplication.java
+++ config-server/src/main/java/config/ServerApplication.java
@@ -1,5 +1,8 @@
+package config;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * @author pingan.yang
@@ -8,8 +11,9 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
  * @create 2018-05-23 15:39
  */
 @SpringBootApplication
+@EnableConfigServer
 public class ServerApplication {
     public static void main(String[] args) {
-        new SpringApplicationBuilder(ServerApplication.class).run(args);
+        new SpringApplicationBuilder(ServerApplication.class).web(true).run(args);
     }
 }
